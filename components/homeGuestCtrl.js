f0dba5d17582a4dc2eec737caab00e085cf8c95d
edanBI <idan.ivri@gmail.com>
2019-06-11 17:21:08 +0300

    random pictures in guest home page

--- components/homeGuestCtrl.js
+++ components/homeGuestCtrl.js
@@ -1,5 +1,20 @@
 
 angular.module('london_app')
-	.controller('homeGuestCtrl', function ($scope) {
-		$scope.firstName = "kaka";
+	.controller('homeGuestCtrl', function ($scope, $http) {
+		$http.get('http://localhost:3000/guest/randExplorePOIs/0')
+			.then((response) => {
+				const ans = response.data;
+				$scope.pois = [];
+				for (let i = 0; i < 3; i++) {
+					$scope.pois.push({
+						name: ans[i].name,
+						picture: ans[i].picture,
+						numViews: ans[i].numViews,
+						poiDescription: ans[i].poiDescription,
+						poiRank: ans[i].poiRank,
+						category: ans[i].category,
+						reviews: ans[i].reviews
+					})					
+				}
+			});
 	});
