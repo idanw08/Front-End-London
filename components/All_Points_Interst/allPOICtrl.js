c3fb9cecae4c7f94af12136bfa782cde23c8a37c
idanw <idanw08@gmail.com>
2019-06-16 16:34:03 +0300

    POI IDAN

--- components/All_Points_Interst/allPOICtrl.js
+++ components/All_Points_Interst/allPOICtrl.js
@@ -1,7 +1,17 @@
 angular.module('london_app')
 .controller('allPointsofInterstController', ['$scope', '$rootScope', '$http', '$location', function ($scope, $rootScope, $http, $location) {
     let self = this;
-    
+    self.selection = {};
+
+     self.picChange = function(pic) {
+        if(pic == 'heartreg.png'){
+            self.photo = 'heartfav.png'
+        }else{
+            self.photo = 'heartreg.png'
+        }
+ };
+
+
     $http.get('http://localhost:3000/user/poi/getAll_POI')
     .then(
         function (response) {
@@ -11,5 +21,6 @@ angular.module('london_app')
         function (error) {
 
         })
+        
 }]);
     //allPointsofInterstController as allPOICtrl
